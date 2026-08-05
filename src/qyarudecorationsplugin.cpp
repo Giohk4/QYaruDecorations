fc3b089ec036544cb83d0e664d745a229ad81c8e
Giohk404 <lhadjnassim440@gmail.com>
2026-08-05 19:29:27 +0100

    Update Copyright

--- src/qyarudecorationsplugin.cpp
+++ src/qyarudecorationsplugin.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2023 Jan Grulich <jgrulich@redhat.com>
+ * Modifications 2026 Nassim Lhadj <lhadjnassim311@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
